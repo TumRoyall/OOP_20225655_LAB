d1637c0c47b930f53a2837a7d07498fee93be7a4
Nguyễn Kim Ngọc <177725608+TumRoyall@users.noreply.github.com>
2024-09-29 21:32:21 +0700

    them file answers.txt

--- Week1/ChoosingOption.java
+++ Week1/ChoosingOption.java
@@ -2,8 +2,8 @@ import javax.swing.JOptionPane;
 
 public class ChoosingOption {
     public static void main(String[] args) {
-        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?", null, 0);
+        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
         JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION?"Yes":"No"));
-        System.exit(option);
+        System.exit(0);
     }    
 }
