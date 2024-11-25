c4b73e6ef48b51cad6641af0cfba7a843d7fbcfc
Nguyễn Kim Ngọc <177725608+TumRoyall@users.noreply.github.com>
2024-11-25 22:35:28 +0700

    ex3

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -10,6 +10,10 @@ public class DigitalVideoDisc {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getCategory() {
         return category;
     }
